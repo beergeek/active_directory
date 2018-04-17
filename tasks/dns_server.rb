1f451ebc7eca6b15e9449fae84728f2a0dabb105
beergeek <brett@puppet.com>
2018-04-17 15:01:00 +1000

    Update error messages

--- tasks/dns_server.rb
+++ tasks/dns_server.rb
@@ -5,7 +5,7 @@ require 'json'
 begin
   cmd_string = 'powershell -command "Get-DNSServer"'
   stdout, stderr, status = Open3.capture3(cmd_string)
-  raise Error, stderr if status != 0
+  raise 'Output not recognised', stderr if status != 0
   puts stdout.strip
   exit 0
 end
